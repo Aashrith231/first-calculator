d9a8444f2e0ff9fd5e0742ab29e16c7bb3f4d08e
aashrith-dev <ashrithboora2@gmail.com>
2025-11-08 03:39:49 +0530

    Implement deleteLastDigit function
    Added a function to delete the last digit from the display.

--- index.js
+++ index.js
@@ -2,6 +2,10 @@
 
 const display = document.getElementById("display");
 
+function deleteLastDigit() {
+    display.value = display.value.slice(0, -1);
+}
+
 function calculate(){
 try {
    display.value= eval(display.value);
